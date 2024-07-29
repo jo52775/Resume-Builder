c2557d35159b2b5b155d9717cf7cff925c4e58bf
iman cheema <cheema.iman2@gmail.com>
2024-07-28 21:05:30 -0400

    Creates initial Login component with form handling

---
+++ app/src/screens/Login.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from "react";
+
+const Login: FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
