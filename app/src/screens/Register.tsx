03425a77a5fdb2ebacb77cd6d223d7d08df78a82
iman cheema <cheema.iman2@gmail.com>
2024-07-30 15:45:04 -0400

    Adds register component info

---
+++ app/src/screens/Register.tsx
@@ -0,0 +1,72 @@
+import React, { FC, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
+
+const Login: FC = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <div className="register-container">
+      <div className="form-section">
+        <h2>Register</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Sign in</button>
+        </form>
+        <div className="links">
+          <p>Already have an account?</p>
+          <Link to="/login" className="link">
+            Login here
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
