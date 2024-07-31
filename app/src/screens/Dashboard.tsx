583b77992375349b3f634b17e0e96c1ad30645d4
Joshua Naraine <somal.naraine@gmail.com>
2024-07-31 13:10:54 -0400

    Create NavBar and begin styling dashboard

--- app/src/screens/Dashboard.tsx
+++ app/src/screens/Dashboard.tsx
@@ -6,14 +6,24 @@ const Dashboard: FC = () => {
 
     // To be changed and styled later
     return(
-        <div>
-            <h1> Welcome to the Dashboard</h1>
-            <div className="dashboard-container">
-            <button> Create New Resume</button>
-            <h2>Saved Resumes</h2>
+        <div className="dashboard-container">
+            <nav className="navbar">
+                <ul className="navbar-items">
+                    <li className="navbar-item"> <Link to="/"> About Us </Link></li>
+                    <li className="navbar-item"> <Link to="/"> My Profile </Link></li>
+                    <li className="navbar-item"> <Link to="/"> Logout </Link></li>
+                </ul>
+            </nav>
+            
+            <div className="dashboard-header">
+                <h1> Welcome to the Dashboard</h1>
+                <button id="resumeCreateButton"> Create New Resume</button>
+            </div>
+
+            <div className="saved-resume-container">
+                <h2>Saved Resumes</h2>
             </div>
         </div>
-        
     )
 }
 
