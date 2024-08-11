675d41adda05bd967449748160970078b173e269
iman cheema <cheema.iman2@gmail.com>
2024-08-11 00:56:50 -0400

    Updates Dashboard to import + use Navbar component

--- app/src/screens/Dashboard.tsx
+++ app/src/screens/Dashboard.tsx
@@ -1,30 +1,22 @@
 import React, { FC, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Dashboard.css";
+import Navbar from "../components/Navbar";
 
 const Dashboard: FC = () => {
+  return (
+    <div className="dashboard-container">
+      <Navbar />
+      <div className="dashboard-header">
+        <h1> Welcome to the Dashboard</h1>
+        <button id="resumeCreateButton"> Create New Resume</button>
+      </div>
 
-    // To be changed and styled later
-    return(
-        <div className="dashboard-container">
-            <nav className="navbar">
-                <ul className="navbar-items">
-                    <li className="navbar-item"> <Link to="/"> About Us </Link></li>
-                    <li className="navbar-item"> <Link to="/"> My Profile </Link></li>
-                    <li className="navbar-item"> <Link to="/"> Logout </Link></li>
-                </ul>
-            </nav>
-            
-            <div className="dashboard-header">
-                <h1> Welcome to the Dashboard</h1>
-                <button id="resumeCreateButton"> Create New Resume</button>
-            </div>
+      <div className="saved-resume-container">
+        <h2>Saved Resumes</h2>
+      </div>
+    </div>
+  );
+};
 
-            <div className="saved-resume-container">
-                <h2>Saved Resumes</h2>
-            </div>
-        </div>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
