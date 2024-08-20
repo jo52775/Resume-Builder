42139b1e1bd1f535fde62501aba4fe36d6a84e86
Joshua Naraine <somal.naraine@gmail.com>
2024-08-20 13:52:05 -0400

    Creates resume preview template and styling

---
+++ app/src/screens/ResumePreview.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./ResumePreview.css"
+
+const ResumePreview: FC = () => {
+
+    return(
+        <div className="resume-preview-container">
+            <h2 className="resume-holder-name"> JOHN SMITH </h2>
+                
+                <div className="resume-section">
+                    <h3 className="section-heading"> SUMMARY </h3>
+                        <p className="section-content"> This is my summary </p>
+                </div>
+                
+                <div className="resume-section">
+                    <h3 className="section-heading"> EXPERIENCE </h3>
+                        <p className="section-content"> This is my experience </p>
+                </div>
+                <div className="resume-section">
+                    <h3 className="section-heading"> EDUCATION </h3>
+                        <p className="section-content"> This is my education </p>
+                </div>
+                <div className="resume-section">
+                    <h3 className="section-heading"> PROJECTS </h3>
+                        <p className="section-content"> These are my projects </p>
+                </div>
+                <div className="resume-section">
+                    <h3 className="section-heading"> SKILLS </h3>
+                        <p className="section-content"> These are my skills </p>
+                </div>
+        </div>
+    )
+}
+
+export default ResumePreview
