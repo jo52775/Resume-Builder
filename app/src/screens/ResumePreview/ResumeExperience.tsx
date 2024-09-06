ca409dafd1aa672c49b65fa2af703e0d1d60ecea
Harish25 <harish09@hotmail.ca>
2024-09-05 23:29:29 -0400

    Added live updates to ResumeExperience, display AI response in KeyResp. textarea

--- app/src/screens/ResumePreview/ResumeExperience.tsx
+++ app/src/screens/ResumePreview/ResumeExperience.tsx
@@ -2,11 +2,30 @@ import React, { FC, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./ResumePreview.css";
 
-const ResumeExperience: FC = () => {
+type ExperienceFormType = {
+  companyName: string,
+  positionTitle:string,
+  keyResponsibilities:string,
+  startDate:string,
+  endDate:string,
+  city:string
+}
+
+interface ExperienceDisplayProps {
+  formData: ExperienceFormType;
+}
+
+const ResumeExperience: FC<ExperienceDisplayProps> = ({formData}) => {
+  const responsibilities = formData.keyResponsibilities.split("\n");
+
   return (
     <div className="resume-section">
       <h3 className="section-heading"> EXPERIENCE </h3>
-      <p className="section-content"> This is my experience </p>
+      <p className="section-content"> {formData.companyName} | {formData.city}</p>
+      <p className="section-content">  {formData.positionTitle} | {formData.startDate} - {formData.endDate}</p>
+      <ul className="section-content"> {responsibilities.map((responsibility, index) => (
+          <li key={index}>{responsibility.trim()}</li>
+        ))}</ul>
     </div>
   );
 };
