c45a6002d78eb16d7f452f0d15126834c7122cf3
iman cheema <cheema.iman2@gmail.com>
2024-09-13 21:50:31 -0400

    Cleans up code in ResumeName.tsx

--- app/src/screens/ResumePreview/ResumeName.tsx
+++ app/src/screens/ResumePreview/ResumeName.tsx
@@ -3,11 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./ResumePreview.css";
 
 const ResumeName: FC = () => {
-    return (
-      
-      <h2 className="resume-holder-name"> JOHN SMITH </h2>
-      
-    );
-  };
-  
-  export default ResumeName;
+  return <h2 className="resume-holder-name"> JOHN SMITH </h2>;
+};
+
+export default ResumeName;
