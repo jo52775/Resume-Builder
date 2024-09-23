f0cd2fcfd25cc9a4bcab10cc59d8886246ffd72d
Joshua Naraine <somal.naraine@gmail.com>
2024-09-23 00:45:35 -0400

    Implements GET request in Dashboard to acquire all user resumes

--- app/src/screens/Dashboard.tsx
+++ app/src/screens/Dashboard.tsx
@@ -1,9 +1,33 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Dashboard.css";
 import Navbar from "../components/Navbar";
 
 const Dashboard: FC = () => {
+  const [resumes, setResumes] = useState([]);
+
+  const fetchSavedResumes = async() => {
+    try {
+      const response = await fetch("http://localhost:5000/get-resumes", {
+        credentials: "include"
+      });
+
+      if(!response.ok){
+        console.log("Failed to fetch resume data.")
+      }
+
+      const data = await response.json();
+      setResumes(data.message);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchSavedResumes();
+  }, [])
+    
   const navigate = useNavigate();
 
   const createResume = async (e: any) => {
@@ -24,9 +48,11 @@ const Dashboard: FC = () => {
 
       <div className="saved-resume-container">
         <h2>Saved Resumes</h2>
+
       </div>
     </div>
   );
 };
 
 export default Dashboard;
+
